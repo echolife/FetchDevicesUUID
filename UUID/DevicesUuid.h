230a69b2e8d45c75e6ea5e0e031f4f72a70d3dc5
王滔 <taowang@MacBook-Pro.lan>
2015-09-19 23:12:59 +0800

    first

---
+++ UUID/DevicesUuid.h
@@ -0,0 +1,13 @@
+//
+//  DevicesUuid.h
+//  UUID
+//
+//  Created by 王滔 on 15/9/19.
+//  Copyright © 2015年 王滔. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DevicesUuid : NSObject
+
+@end
---
+++ UUID/DevicesUuid.m
@@ -0,0 +1,13 @@
+//
+//  DevicesUuid.m
+//  UUID
+//
+//  Created by 王滔 on 15/9/19.
+//  Copyright © 2015年 王滔. All rights reserved.
+//
+
+#import "DevicesUuid.h"
+
+@implementation DevicesUuid
+
+@end
